7caf60697757484de347a557bad6855f7fd5fb6b
Tania <tanialorences@gmail.com>
2024-11-01 16:23:58 +0100

    2º commit: cambios layout app

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
       <h1>Ejercicios de React</h1>
       <li><h2>Ejercicio 1: Componente Contador</h2></li>
       <Contador />
-      <li><h2>Ejercicio 2: Formulario Simple</h2></li>
+      <li><h2>Ejercicio 2: Formulario simple</h2></li>
       <Formulario />
-      <li><h2>Ejercicio 3: Toggle de Visibilidad</h2></li>
+      <li><h2>Ejercicio 3: Toggle de visibilidad</h2></li>
       <ToggleVisibilidad />
-      <li><h2>Ejercicio 4: Lista de Tareas</h2></li>
+      <li><h2>Ejercicio 4: Lista de tareas</h2></li>
       <ListaTareas />
-      <li><h2>Ejercicio 5: Buscador de Productos</h2></li>
+      <li><h2>Ejercicio 5: Buscador de productos</h2></li>
       <BuscadorProductos />
       </ul>
     </div>
